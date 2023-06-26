cea6eb19120700448fb1abdf0fadc5c46e6de597
TheJose <joseenrique123438@gmail.com>
2023-06-26 16:17:59 -0500

    implementacion de efectos en nav-bar

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-//Preloader
+/*====================Loader=================================
+Este código es para un loader que se mostrará
+durante 3 segundos antes de desvanecerse y
+permitir que se muestre el contenido del sitio web.*/
+
 document.addEventListener("DOMContentLoaded", function() {
     var loaderContainer = document.getElementById("loader");
     
@@ -9,17 +13,28 @@ document.addEventListener("DOMContentLoaded", function() {
   
     document.body.style.overflow = "hidden";
 });
-//fin
+//=====================Fin Loader============================
+
+
+
+/*=======================MENU RESPONSIVE-max width:980px======================
+Este código define dos funciones para mostrar y ocultar un menú,
+y para seleccionar una opción del menú. La primera función alterna
+la visibilidad del menú agregando o quitando una clase CSS al
+elemento de navegación, y también agrega o quita una clase al cuerpo
+para habilitar o deshabilitar el desplazamiento. La segunda función
+simplemente oculta el menú después de que se ha seleccionado una opción.*/
 
 let menuVisible = false;
 
-//Funcion que oculta o muesta el menu
 function mostrarOcultarMenu(){
     if(menuVisible){
         document.getElementById("nav").classList ="";
+        document.body.classList.remove("no-scroll"); // Habilitar scroll
         menuVisible = false;
     } else{
         document.getElementById("nav").classList ="responsive";
+        document.body.classList.add("no-scroll"); // Deshabilitar scroll
         menuVisible = true;
     }
 }
@@ -27,10 +42,16 @@ function mostrarOcultarMenu(){
 function seleccionar(){
     //Oculto el menu una vez que selecciono una opcion
     document.getElementById("nav").classList = "";
+    document.body.classList.remove("no-scroll"); // Habilitar scroll
     menuVisible = false;
 }
+/*===============================FIN MENU====================================*/
 
-//Funcion que aplica las animaciones de las SKILLS
+
+/*===============================EFECTO ANIMACION===============================
+Esta función activa un efecto visual en la sección de habilidades de la página
+web cuando se desplaza hacia ella*/
+
 function efectoSkills(){
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
@@ -46,8 +67,74 @@ function efectoSkills(){
         habilidades[7].classList.add("dedicacion");
     }
 }
+//=============================FIN ANIMACION======================================
 
-//Detecto el scrolling para aplicar la animacion de la bara de habilidades
+
+/*======================FUNCION DE NAVEGACION DE SECCIONES=====================
+regar la clase "active" a la sección actual en la barra de navegación. La función
+"actualizarSeccionActual()" recorre todas las secciones y agrega la clase "active"
+a la etiqueta "a" correspondiente a la sección actual. Además, se extrae el valor
+del hash de la URL para agregar la clase "active" a la sección correspondiente en
+la barra de navegación.*/
+
+      // Obtener la URL actual
+      $(document).ready(function() {
+        // Agregar la clase "active" a la sección actual al cargar la página
+        actualizarSeccionActual();
+         // Agregar la clase "active" a la sección actual al desplazarse por la página
+        $(window).scroll(function() {
+          actualizarSeccionActual();
+        });
+      });
+      function actualizarSeccionActual() {
+        // Obtener la posición actual del desplazamiento
+        var scrollPos = $(document).scrollTop();
+         // Recorrer todas las secciones y determinar cuál es visible en la pantalla
+        $('#nav a').each(function() {
+          var ref = $(this).attr('href');
+          var seccionPos = $(ref).offset().top;
+          var seccionAltura = $(ref).outerHeight();
+          if (seccionPos <= scrollPos && (seccionPos + seccionAltura) > scrollPos) {
+            // Agregar la clase "active" a la etiqueta "a" correspondiente a la sección actual
+            $(this).addClass('active');
+          } else {
+            // Eliminar la clase "active" de la etiqueta "a" correspondiente a las secciones no visibles
+            $(this).removeClass('active');
+          }
+        });
+      }
+      
+      var url = window.location.href;
+       // Obtener el valor del hash de la URL (por ejemplo, "#inicio")
+      var hash = url.substring(url.indexOf("#"));
+       // Agregar la clase "active" a la etiqueta "a" correspondiente a la sección actual
+      $('#nav a[href="'+hash+'"]').addClass('active');
+//=============================================FIN=====================================================
+
+
+/*=============================Animación de habilidades al desplazarse=============================
+Detecto el scrolling para aplicar la animacion de la bara de habilidades.
+Esto permite que la animación se active cuando el usuario llega a cierto punto de la página*/
+
 window.onscroll = function(){
     efectoSkills();
 }
+//=========================================FIN ANIMACION=============================================
+
+
+/*================Técnica de retraso para envío de formulario de contacto===================
+ Este código es para un formulario de contacto que tiene una función de retraso de 3 segundos
+ antes de enviar el formulario. Cuando el usuario hace clic en el botón de envío, se deshabilita
+ temporalmente para evitar envíos duplicados y se agrega un retraso antes de enviar el formulario.*/
+ 
+document.querySelector('form[name="formulario-contacto"]').addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita que se envíe el formulario de inmediato
+    
+    document.getElementById("submitButton").disabled = true; // Deshabilita el botón de envío
+    
+    // Agrega un retraso de 3 segundos antes de enviar el formulario
+    setTimeout(function() {
+      event.target.submit(); // Envía el formulario después del retraso
+    }, 3000);
+  });
+  //=====================================FIN FORMULARIO==================================================
